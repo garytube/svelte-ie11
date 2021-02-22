fa89c4b9acf14c82caef0f9e9fee832ebe614474
Gerald Scholz <g@geryy.io>
2021-02-22 10:34:40 +0100

    added svelte example + async fetch

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,37 @@
 <script>
-  //
+	import FetchExample from './FetchExample.svelte'
+	export let name
 </script>
 
-<style>
-</style>
+<main>
+	<h1>Hello {name}!</h1>
+	<h2>Hello Internet Explorer 11</h2>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
 
-<main>hello IE 11</main>
+	<FetchExample />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
